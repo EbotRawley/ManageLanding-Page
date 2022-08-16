223275ce72300a1cb6294a562bfeea221a121758
Rawley <74854100+EbotRawley@users.noreply.github.com>
2022-08-16 17:47:35 +0100

    Add files via upload

--- manage-landing-page/script.js
+++ manage-landing-page/script.js
@@ -1,57 +1,57 @@
 // Scroll To Top On Load
 
-window.onunload = function(){
+window.onunload = function () {
   window.scrollTo(0, 0)
 }
 
 // Carousel Slider
 
-$(document).ready(function() {
- 
-    $(".owl-carousel").owlCarousel();
-   
-  });
+$(document).ready(function () {
+
+  $(".owl-carousel").owlCarousel();
+
+});
 
 $(".owl-carousel").owlCarousel({
- 
-    //Basic Speeds
-    slideSpeed : 200,
-    paginationSpeed : 800,
- 
-    //Autoplay
-    autoPlay : false,
-    goToFirst : true,
-    goToFirstSpeed : 1000,
- 
-    // Navigation
-    navigation : true,
-    navigationText : ["<",">"],
-    pagination : true,
-    paginationNumbers: false,
- 
-    // Responsive
-    responsive: true,
-    items : 2.5,
-    itemsDesktop : [1199,2.5],
-    itemsDesktopSmall : [980,2],
-    itemsTablet: [768,1],
-    itemsMobile : [375,1]
- 
+
+  //Basic Speeds
+  slideSpeed: 200,
+  paginationSpeed: 800,
+
+  //Autoplay
+  autoPlay: false,
+  goToFirst: true,
+  goToFirstSpeed: 1000,
+
+  // Navigation
+  navigation: true,
+  navigationText: ["<", ">"],
+  pagination: true,
+  paginationNumbers: false,
+
+  // Responsive
+  responsive: true,
+  items: 2.5,
+  itemsDesktop: [1199, 2.5],
+  itemsDesktopSmall: [980, 2],
+  itemsTablet: [768, 1],
+  itemsMobile: [375, 1]
+
 })
 
 // Email validation
 
-function validateEmail(email){
+function validateEmail(email) {
   var input = document.getElementById("email")
   var err_msg = document.querySelector(".email_error")
   var regex = /^[a-zA-Z0-9.!#$%'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  if(email.value.match(regex)){
+  if (email.value.match(regex)) {
     input.style.color = ""
     err_msg.style.display = "none"
     input.style.outline = "none"
     return true
   }
-  else{
+  else {
     input.style.color = "red"
     err_msg.style.display = "block"
     input.style.outline = "1px solid red"
@@ -62,21 +62,12 @@ function validateEmail(email){
 
 // Show/Hide Menu
 
+const overlay = document.getElementById("menu_overlay")
+const links = document.getElementById("links")
+const menu = document.getElementById("menu")
+
 function toggleMenu() {
-  var overlay = document.getElementById("menu_overlay")
-  var links = document.getElementById("links")
-  var menu = document.getElementById("menu")
   links.classList.toggle("responsive")
   menu.classList.toggle("close")
-  if(menu.classList == close)
-    overlay.style.display = "block"
-  else
-    overlay.style.display = "none"
-}
-
-// close menu on click out
-
-function overlay(){
-  var overlay = document.getElementById("menu_overlay")
-  overlay.style.display = "none"
+  overlay.classList.toggle("ovl")
 }
